6ac2b251017f452f41da202caed972345a79166a
Matus Tib <matus.tibensky@gmail.com>
2022-09-28 12:23:50 +0000

    External streamlit sources added do readme

--- ab_testing/constants.py
+++ ab_testing/constants.py
@@ -2,4 +2,4 @@ from typing import Literal
 
 DISTRIBUTIONS: list = ["expon", "lognorm"]
 target_col: str = "total_wins_spend"
-client_name: Literal["bingo_aloha", "terra_genesis", "spongebob", "ultimex", "knighthood", "idle_mafia", "homw"] = "idle_mafia"
+client_name: Literal["bingo_aloha", "terra_genesis", "spongebob", "ultimex", "knighthood", "idle_mafia", "homw"] = "bingo_aloha"
