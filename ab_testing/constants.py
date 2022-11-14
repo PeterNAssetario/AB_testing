68d158083d374d0642ea3dc4f48763c6fba20991
Jakub Chudik <jakubchudik97@gmail.com>
2022-11-14 16:16:09 +0100

    Add new queries, black

--- ab_testing/constants.py
+++ ab_testing/constants.py
@@ -2,4 +2,13 @@ from typing import Literal
 
 DISTRIBUTIONS: list = ["expon", "lognorm"]
 target_col: str = "total_wins_spend"
-client_name: Literal["bingo_aloha", "terra_genesis", "spongebob", "ultimex", "knighthood", "idle_mafia", "homw"] = "idle_mafia"
+client_name: Literal[
+    "bingo_aloha",
+    "terra_genesis",
+    "spongebob",
+    "ultimex",
+    "knighthood",
+    "idle_mafia",
+    "homw",
+    "terra_2",
+] = "ultimex"
