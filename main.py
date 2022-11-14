b844fa431b7f5597849abd2a35de13c1e63efa44
Jakub Chudik <jakubchudik97@gmail.com>
2022-11-14 16:22:30 +0100

    Black

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ acquire_initial_data = AcquireData(client=client_name, fname=f"{client_name}_dat
 initial_data = acquire_initial_data.acquire_data()
 
 fit_dist = FitDistribution(fname=f"{client_name}_distribution_fit.p")
-best_distribution = fit_dist.fit(initial_data.loc[initial_data[target_col] > 0], target_col)
+best_distribution = fit_dist.fit(
+    initial_data.loc[initial_data[target_col] > 0], target_col
+)
 
 result = ProducePredictions()
 results_conversion = result.produce_results_conversion(initial_data)
@@ -22,7 +24,11 @@ output_df["Conversion"] = [
     results_conversion[0]["expected_loss"],
     results_conversion[1]["expected_loss"],
 ]
-output_df["Revenue"] = [results_revenue[0]["prob_being_best"], results_revenue[0]["expected_loss"], results_revenue[1]["expected_loss"]]
+output_df["Revenue"] = [
+    results_revenue[0]["prob_being_best"],
+    results_revenue[0]["expected_loss"],
+    results_revenue[1]["expected_loss"],
+]
 
 print(f"For client {client_name} data follows {best_distribution} distribution.")
 print(output_df)
