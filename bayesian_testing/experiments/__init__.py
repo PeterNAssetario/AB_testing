a5e5d8691b569ab5cc71f26cc1f499ae012b39bb
Karolina Mojzisova <ge69buv@mytum.de>
2023-02-10 12:04:44 +0100

    update init files and requirements

--- bayesian_testing/experiments/__init__.py
+++ bayesian_testing/experiments/__init__.py
@@ -1,11 +1,7 @@
 from .binary import BinaryDataTest
-from .normal import NormalDataTest
 from .delta_lognormal import DeltaLognormalDataTest
-from .discrete import DiscreteDataTest
 
 __all__ = [
     "BinaryDataTest",
-    "NormalDataTest",
     "DeltaLognormalDataTest",
-    "DiscreteDataTest",
 ]
--- bayesian_testing/metrics/__init__.py
+++ bayesian_testing/metrics/__init__.py
@@ -1,13 +1,6 @@
-from .evaluation import (
-    eval_bernoulli_agg,
-    eval_normal_agg,
-    eval_delta_lognormal_agg,
-    eval_numerical_dirichlet_agg,
-)
+from .evaluation import eval_bernoulli_agg, eval_delta_lognormal_agg
 
 __all__ = [
     "eval_bernoulli_agg",
-    "eval_normal_agg",
     "eval_delta_lognormal_agg",
-    "eval_numerical_dirichlet_agg",
 ]
