3d60e532d4c01f375f3b0679eb4b3b146f51ff7b
Karolina Mojzisova <ge69buv@mytum.de>
2023-02-10 16:40:14 +0100

    rename

---
+++ streamlit_small_testing_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Testing app",
+    page_icon="📊",
+    initial_sidebar_state="expanded",
+    layout="wide",
+)
+
+st.write(
+    """
+# 📊 Testing whether stuff works
+"""
+)
