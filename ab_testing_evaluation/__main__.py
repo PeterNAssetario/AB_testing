c74dd81df9e268ed24855ddc035e7aafd634d725
Matus Kislan <matuskislan@gmail.com>
2023-02-22 13:47:06 +0000

    add configure_sentry

--- ab_testing_evaluation/__main__.py
+++ ab_testing_evaluation/__main__.py
@@ -1,7 +1,9 @@
 from testing import AbTestEvaluationConfig, run_ab_testing, upload_output_to_s3
-
+from ml_lib.util.sentry import configure_sentry
 
 if __name__ == "__main__":
+    configure_sentry()
+
     config = AbTestEvaluationConfig()
     ab_testing_output = run_ab_testing(config)
     upload_output_to_s3(
